ca443145e74787b6fcd8249a01ba102a36cceec1
edwynss14 <elezamarodriguez@danielcastelao.org>
2026-09-25 12:33:54 +0200

    Nivel 2 completado

--- src/Lanzador.java
+++ src/Lanzador.java
@@ -1,13 +1,24 @@
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 
 public class Lanzador {
     public int ejecutarFactor(String numero){
         try{
             ProcessBuilder pb = new ProcessBuilder("factor", numero); //Aquí el ProcessBuilder crea y lanza programas o comandos externos desde el propio código Java
-            pb.inheritIO(); // Esto es la salida que se imprime en la terminal, es decir, lo que estaría en consola en linux solo que en la terminal de Java
             Process p = pb.start(); // Java incia, es decir, el sistema operativo ejecuta el comando factor y Java se queda esperando a la respuesta
+            BufferedReader salidaOk = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String linea;
+            while((linea = salidaOk.readLine()) != null){
+                System.out.println("[OK] " + linea);
+            }
+
+            BufferedReader salidaError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            while((linea = salidaError.readLine()) != null){
+                System.out.println("[ERROR] " + linea);
+            }
             return p.waitFor(); //Espera a que el comando termine de ejecutarse en Linux, a su vez sale el código, si es 0 salió todo bien, si es 1 es porque ha ocurrido un error
         } catch (Exception e){
             return 1;
